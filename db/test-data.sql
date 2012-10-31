6101f06f24d8fd1cdd55fda567f269647c93542d
marko soomets <marko.soomets@gmail.com>
2012-10-31 14:51:17 +0000

    Database Upgrade

--- db/test-data.sql
+++ db/test-data.sql
@@ -1,22 +1,23 @@
+
+
 use blogaggregator;
 
-insert into user (email) value('test@gmail.com');
+insert ignore into user(email) value('test@gmail.com');
 
-insert into bloglist (listName) values ('Esimene List');
+insert into bloglist(listName,email) values ('Esimene List','test@gmail.com');
 
-INSERT INTO blog (title, htmlUrl, xmlUrl) VALUES ('Booking it some more', 'http://bookingitsomemore.blogspot.com/', 'http://bookingitsomemore.blogspot.com/feeds/posts/default');
-INSERT INTO blog (title, htmlUrl, xmlUrl) VALUES ('Grafomania', 'http://grafomania.wordpress.com', 'http://grafomania.wordpress.com/feed/');
-INSERT INTO blog (title, htmlUrl, xmlUrl) VALUES ('Vinttikamarissa', 'http://vinttikamarissa.blogspot.com/', 'http://vinttikamarissa.blogspot.com/feeds/posts/default');
-INSERT INTO blog (title, htmlUrl, xmlUrl) VALUES ('Tuulevin lukublogi', 'http://tuulevi.wordpress.com', 'http://tuulevi.wordpress.com/feed/');
+INSERT INTO blog (title, xmlUrl) VALUES ('Booking it some more', 'http://bookingitsomemore.blogspot.com/feeds/posts/default');
+INSERT INTO blog (title, xmlUrl) VALUES ('Grafomania', 'http://grafomania.wordpress.com/feed/');
+INSERT INTO blog (title, xmlUrl) VALUES ('Vinttikamarissa', 'http://vinttikamarissa.blogspot.com/feeds/posts/default');
+INSERT INTO blog (title, xmlUrl) VALUES ('Tuulevin lukublogi', 'http://tuulevi.wordpress.com/feed/');
 
 insert into tag (name) values ('Finnish');
 insert into tag (name) values ('Books');
 
-insert into user_bloglist value(1,1);
 
-insert into blog_bloglist values (1,1);
-insert into blog_bloglist values (2,1);
-insert into blog_bloglist values (3,1);
+insert into blog_bloglist values (1,2);
+insert into blog_bloglist values (2,2);
+insert into blog_bloglist values (3,2);
 insert into blog_bloglist values (4,1);
 
 insert into blog_tag values (1,1);
