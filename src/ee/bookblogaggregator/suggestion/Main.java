ce7f73f52d643563a1f12691373471de4b415b35
karlrank <karlrank@localhost>
2012-11-22 13:42:54 +0200

    Fixes issue 33 Fixes issue 37 Fixes issue 38 Fixes issue 39

---
+++ src/ee/bookblogaggregator/suggestion/Main.java
@@ -0,0 +1,41 @@
+package ee.bookblogaggregator.suggestion;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
+
+import ee.bookblogaggregator.data.*;
+import ee.bookblogaggregator.servlet.TagManagerServlet;
+
+public class Main {
+	public static void updateSuggestions (String email) {
+		final Logger log = Logger.getLogger(TagManagerServlet.class.getName());
+		
+		List<Tag> tags = Functions.getTags(email);
+		List<Blog> blogs = Functions.getBlogs(email);
+		
+		for (Iterator<Blog> iterator = blogs.iterator(); iterator.hasNext();) {
+			Blog blog = (Blog) iterator.next();
+			blog.setRating(0);
+			for (Iterator<Tag> iterator2 = tags.iterator(); iterator2.hasNext();) {
+				Tag tag = (Tag) iterator2.next();
+				for (Iterator<Tag> iterator3 = blog.getTags().iterator(); iterator3.hasNext();) {
+					Tag tag2 = (Tag) iterator3.next();
+					if (tag.getId() == tag2.getId()) {
+						blog.setRating(blog.getRating() + tag.getRating());
+					}
+				}
+			}
+			blog.setRating((blog.getRating() / blog.getTags().size()) * blog.getPopularity());			
+		}
+		
+		for (Iterator<Blog> iterator = blogs.iterator(); iterator.hasNext();) {
+			Blog blog = (Blog) iterator.next();
+			log.info("Blogs: " + blog);
+		}
+		
+		
+	}
+	
+	
+}
